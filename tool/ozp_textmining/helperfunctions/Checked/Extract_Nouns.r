939806574b4d9f711544b880d69110f9c5b0a996
Ralph <horn.ralph@gmail.com>
2018-06-19 15:45:26 +0200

    updated noun extraction method

---
+++ tool/ozp_textmining/helperfunctions/Checked/Extract_Nouns.r
@@ -0,0 +1,20 @@
+# load standford coreNLP package
+library(phrasemachine)
+library(tidyverse)
+
+#
+# >> extract_nouns <<
+# Input: a description string
+# Output: all the nouns in the string
+#
+
+extract_nouns <- function(description) {
+    output =  POS_tag_documents(description)
+
+    output = as.data.frame(output$Document_1)
+
+    output = filter(output, tags == "NN" | tags == "NNP")
+
+
+    return(output)
+}
