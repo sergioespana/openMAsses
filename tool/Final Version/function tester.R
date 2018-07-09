83a7b78bee8b66b8efa982744923cb19f2b7f268
Ralph <horn.ralph@gmail.com>
2018-07-09 15:09:26 +0200

    fixed noun adjective keyword generation. made start with first longlist version that can be used in tool v2

--- tool/Final Version/function tester.R
+++ tool/Final Version/function tester.R
@@ -15,3 +15,15 @@ test_extract_nouns <- function() {
     return(output)
 }
 
+test_extract_nouns_adjectives <- function() {
+
+    # load the method
+    source("functions/generate_keywords_noun_adjective.r")
+    input = "the female executive ratio is defined by the ratio of female executives to male executives whereas the happy executives are excluded."
+
+    output = ozp_generate_keywords_nouns_adjectives(input)
+    return(output)
+}
+
+
+test = test_extract_nouns_adjectives()
