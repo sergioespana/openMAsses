585f350c8e6c241a9eca9cd5261d001b4618637f
4071425 <M.N.C.Keijdener@students.uu.nl>
2019-04-29 10:18:01 +0200

    added .exe exectuter test

--- tool/testscript.R
+++ tool/testscript.R
@@ -1,16 +1,5 @@
 plot <- function(){
-  a <- c(1,2,3,6)
-  b <- c(2,3,5,7)
-  c <- c(2,4,6,8)
-  
-  print(a/b * 2)
-  
-  mat <- cbind(a,b,c)
-  
-  print(which(mat > 2))
-  mat[which(mat > 2)] <- 100
-  
-  print(mat)
+  system(shQuote("C:/Users/Melchior/PycharmProjects/ThesisV3/dist/main/main.exe" , type = "cmd" ))
 }
 plot()
 rm(plot)
